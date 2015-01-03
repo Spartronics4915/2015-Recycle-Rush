d6a3654f25595dc575abaeb0dc383958b01b4bcc
Phroa <Phroa@users.noreply.github.com>
2015-01-03 13:47:03 -0800

    Insert error to test Travis's error checking

--- src/com/spartronics4915/recyclerush/Main.java
+++ src/com/spartronics4915/recyclerush/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Please remove this once build season officially starts.");
+		System.out.println("Please remove this once build season officially starts.")
 
 	}
 
