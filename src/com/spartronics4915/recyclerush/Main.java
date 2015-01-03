66af28b5c0854766e6bf4b603befc4b51a448877
Phro a <nr.phroa@gmail.com>
2015-01-03 13:00:43 -0800

    Initial commit to ensure the repository works

---
+++ src/com/spartronics4915/recyclerush/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.spartronics4915.recyclerush;
+
+/**
+ *
+ * Test class. Please remove this once build season officially starts.
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args Command-line arguments to program
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Please remove this once build season officially starts.");
+
+	}
+
+}
