bbbaf79d26ccff631e6bc278fd67cb5101cedabf
TarkanAl-Kazily <tarkucar@mac.com>
2015-01-24 14:07:16 -0800

    Added the Compressor subsystem which won't be used for anything important, and added the Grabber subsystem which we will modify.

---
+++ RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/subsystems/Compressor.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc4915.MecanumDrive.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Compressor extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	//Compressors this year do not require a manual start or stop feature. They automatically are run when we instantiate a solenoid.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
