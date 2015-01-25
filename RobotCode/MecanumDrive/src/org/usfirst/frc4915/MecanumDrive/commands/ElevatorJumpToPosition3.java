b4e6f9b1436c699abd7145934593fd2d2f6da1f7
TarkanAl-Kazily <tarkucar@mac.com>
2015-01-25 14:56:05 -0800

    Added the remaining ElevatorJumpToPosition commands

---
+++ RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/commands/ElevatorJumpToPosition3.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc4915.MecanumDrive.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ElevatorJumpToPosition3 extends Command {
+
+    public ElevatorJumpToPosition3() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
