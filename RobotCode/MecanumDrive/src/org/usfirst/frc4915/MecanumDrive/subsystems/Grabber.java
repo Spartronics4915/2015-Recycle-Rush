f1442e5e02f7b7769654b0fae5ffec5909407484
Olivia Pells <oliviapells@gmail.com>
2015-01-29 21:11:45 -0800

    Commiting before pulling master branch

--- RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/subsystems/Grabber.java
+++ RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/subsystems/Grabber.java
@@ -13,8 +13,9 @@ public class Grabber extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
-	DoubleSolenoid grabberSolenoid = RobotMap.grabberSolenoid;
-	
+	DoubleSolenoid mommaSolenoid = RobotMap.mommaSolenoid;
+    DoubleSolenoid babySolenoid = RobotMap.babySolenoid;
+
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
