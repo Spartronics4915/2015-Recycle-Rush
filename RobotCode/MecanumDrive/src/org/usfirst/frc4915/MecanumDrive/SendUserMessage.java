c9207c115735b0fdf22ba35cf7460718eb084a7e
Phro a <nr.phroa@gmail.com>
2015-01-30 16:18:07 -0800

    Fix compiler warnings and format code

--- RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/SendUserMessage.java
+++ RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/SendUserMessage.java
@@ -1,9 +1,9 @@
 package org.usfirst.frc4915.MecanumDrive;
 
-import edu.wpi.first.wpilibj.smartdashboard.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SendUserMessage {
-	public static void displayMessage() {
-		SmartDashboard.putString("DB/String 0", "Message to user");
-	}
+    public static void displayMessage() {
+        SmartDashboard.putString("DB/String 0", "Message to user");
+    }
 }
