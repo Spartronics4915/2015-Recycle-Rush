87ce6388c53b788836f9d754f095b3158333d52d
TarkanAl-Kazily <tarkucar@mac.com>
2015-01-30 19:35:33 -0800

    Robot actually drives. Added comments to MecanumDrive commands.

--- RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/Robot.java
+++ RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/Robot.java
@@ -41,7 +41,7 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-    RobotMap.init();
+    	RobotMap.init();
         driveTrain = new DriveTrain();
         elevator = new Elevator();
         // OI must be constructed after subsystems. If the OI creates Commands 
