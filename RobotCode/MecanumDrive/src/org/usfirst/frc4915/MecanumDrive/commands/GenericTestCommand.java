b2a4fac2e31a7b5ea5ff07c899c5f515114e252c
Phro a <nr.phroa@gmail.com>
2015-02-07 14:35:04 -0800

    Create a simple command to use in testing things

---
+++ RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/commands/GenericTestCommand.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc4915.MecanumDrive.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class GenericTestCommand extends Command {
+	
+	int count = 0;
+	int i;
+	String message;
+
+    public GenericTestCommand(int count, String message) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	this.count = count;
+    	this.message = message;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	System.out.printf("Saying \"%s\" %i times.%n", message, count);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	System.out.printf("%s (%i/%i)", message, i, count);
+    	i++;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return i == count;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
