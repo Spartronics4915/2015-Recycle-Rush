ad51020a2c75e72b7a038c127206029fbacf3edc
TarkanAl-Kazily <tarkucar@mac.com>
2015-02-07 13:34:55 -0800

    Fixed call to getElevatorStick

--- RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/commands/ElevatorFineTune.java
+++ RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/commands/ElevatorFineTune.java
@@ -25,7 +25,7 @@ public class ElevatorFineTune extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	elevator.moveAtSpeed(Robot.oi.elevatorStick());
+    	elevator.moveAtSpeed(Robot.oi.elevatorStick);
     }
 
     // Make this return true when this Command no longer needs to run execute()
