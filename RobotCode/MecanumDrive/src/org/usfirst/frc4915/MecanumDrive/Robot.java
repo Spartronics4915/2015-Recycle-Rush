07c66c549baeb5ee91424d6ebd76e8e5b5ef7a2f
TarkanAl-Kazily <tarkucar@mac.com>
2015-02-17 11:30:43 -0800

    Fixed Robot.java's getElevatorNumber with getPositionNumber

--- RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/Robot.java
+++ RobotCode/MecanumDrive/src/org/usfirst/frc4915/MecanumDrive/Robot.java
@@ -254,7 +254,7 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putNumber("Elevator D", elevator.winch.getD());
 		SmartDashboard.putNumber("Maximum height value: ", Elevator.maximumPotentiometerValue);
 		SmartDashboard.putNumber("Minimum height value: ", Elevator.minimumPotentiometerValue);
-		SmartDashboard.putNumber("Position Number of Elevator: ", Robot.elevator.getElevatorLevel());
+		SmartDashboard.putNumber("Position Number of Elevator: ", Robot.elevator.getPositionNumber());
 		SmartDashboard.putBoolean("Safety Enabled", Elevator.SAFETY);
 		
 
